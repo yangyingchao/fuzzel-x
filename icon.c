e6ab64b0616c33e7bf080e22411b69fc52d8509a
Daniel Eklöf <daniel@ekloef.se>
2019-03-01 19:26:59 +0100

    icon: look for theme in all the places

--- icon.c
+++ icon.c
@@ -125,22 +125,21 @@ parse_theme(FILE *index, struct icon_theme *theme)
     free(type);
 }
 
-struct icon_theme *
-icon_load_theme(const char *name)
+static struct icon_theme *
+load_theme_in(const char *name, int data_dir_fd)
 {
-    /* TODO: XDG_DATA_HOME, .local/share, XDG_DATA_DIRS */
-
-    int data_dir_fd = -1;
     int theme_dir_fd = -1;
     int index_fd = -1;
     FILE *index = NULL;
     struct icon_theme *theme = NULL;
 
+#if 0
     data_dir_fd = open("/usr/share/icons", O_RDONLY);
     if (data_dir_fd == -1) {
         LOG_ERRNO("%s: failed to open", "/usr/share/icons");
         goto out;
     }
+#endif
 
     theme_dir_fd = openat(data_dir_fd, name, O_RDONLY);
     if (theme_dir_fd == -1) {
@@ -171,14 +170,113 @@ icon_load_theme(const char *name)
         close(index_fd);
     if (theme_dir_fd != -1)
         close(theme_dir_fd);
+#if 0
     if (data_dir_fd != -1)
         close(data_dir_fd);
+#endif
+    return theme;
+}
+
+struct icon_theme *
+icon_load_theme(const char *name)
+{
+    struct icon_theme *theme = NULL;
+
+    /*
+     * First, look in the user's private XDG_DATA_HOME (falling back
+     * to .local/share if XDG_DATA_HOME hasn't been set)
+     */
+
+    const char *xdg_data_home = getenv("XDG_DATA_HOME");
+    if (xdg_data_home != NULL) {
+        int data_fd = open(xdg_data_home, O_RDONLY);
+        int fd = data_fd != -1 ? openat(data_fd, "icons", O_RDONLY) : -1;
+
+        if (fd != -1) {
+            theme = load_theme_in(name, fd);
+            close(fd);
+        }
+
+        if (data_fd != -1)
+            close(data_fd);
+    } else {
+        const char *home = getenv("HOME");
+        int home_fd = open(home, O_RDONLY);
+        int fd = home_fd != -1
+            ? openat(home_fd, ".local/share/icons", O_RDONLY)
+            : -1;
+
+        if (fd != -1) {
+            theme = load_theme_in(name, fd);
+            close(fd);
+        }
+
+        if (home_fd != -1)
+            close(home_fd);
+    }
+
+    if (theme != NULL)
+        return theme;
+
+    /*
+     * Second, look through the directories specified in XDG_DATA_DIRS
+     * (falling back to /usr/local/share:/usr/share if XDG_DATA_DIRS
+     * hasn't been set).
+     */
+
+    const char *_xdg_data_dirs = getenv("XDG_DATA_DIRS");
+    if (_xdg_data_dirs != NULL) {
+
+        char *xdg_data_dirs = strdup(_xdg_data_dirs);
+
+        for (const char *data_dir = strtok(xdg_data_dirs, ":");
+             data_dir != NULL;
+             data_dir = strtok(NULL, ":"))
+        {
+            int fd_base = open(data_dir, O_RDONLY);
+            int fd = fd_base != -1 ? openat(fd_base, "icons", O_RDONLY) : -1;
+
+            if (fd != -1) {
+                theme = load_theme_in(name, fd);
+                close(fd);
+            }
+
+            if (fd_base != -1)
+                close(fd_base);
+
+            if (theme != NULL)
+                break;
+        }
+        free(xdg_data_dirs);
+
+    } else {
+        static const char *const default_paths[] = {
+            "/usr/local/share/icons",
+            "/usr/share/icons",
+        };
+        static const size_t cnt = sizeof(default_paths) / sizeof(default_paths[0]);
+
+        for (size_t i = 0; i < cnt; i++) {
+            int fd = open(default_paths[i], O_RDONLY);
+            if (fd != -1) {
+                theme = load_theme_in(name, fd);
+                close(fd);
+            }
+
+            if (theme != NULL)
+                break;
+        }
+    }
+
     return theme;
 }
 
 void
 icon_theme_destroy(struct icon_theme *theme)
 {
+    if (theme == NULL)
+        return;
+
     free(theme->path);
 
     tll_foreach(theme->dirs, it) {
