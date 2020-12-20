902f8209cfe20dfa001e422ad2000ca32f4882b2
Jan Beich <jbeich@FreeBSD.org>
2020-12-20 14:06:32 +0000

    shm: unbreak build without memfd_create
    New FreeBSD versions have memfd_create but other BSDs don't.
    
    ../shm.c:8:10: fatal error: 'linux/memfd.h' file not found
     #include <linux/memfd.h>
              ^~~~~~~~~~~~~~~
    ../shm.c:66:15: error: implicit declaration of function 'memfd_create' is invalid in C99 [-Werror,-Wimplicit-function-declaration]
        pool_fd = memfd_create("fuzzel-wayland-shm-buffer-pool", MFD_CLOEXEC);
                  ^
    ../shm.c:66:62: error: use of undeclared identifier 'MFD_CLOEXEC'
        pool_fd = memfd_create("fuzzel-wayland-shm-buffer-pool", MFD_CLOEXEC);
                                                                 ^

--- shm.c
+++ shm.c
@@ -5,7 +5,7 @@
 
 #include <sys/types.h>
 #include <sys/mman.h>
-#include <linux/memfd.h>
+#include <fcntl.h>
 
 #include <tllist.h>
 
@@ -64,7 +64,16 @@ shm_get_buffer(struct wl_shm *shm, int width, int height)
     cairo_t *cairo = NULL;
 
     /* Backing memory for SHM */
+#if defined(MEMFD_CREATE)
     pool_fd = memfd_create("fuzzel-wayland-shm-buffer-pool", MFD_CLOEXEC);
+#elif defined(__FreeBSD__)
+    // memfd_create on FreeBSD 13 is SHM_ANON without sealing support
+    pool_fd = shm_open(SHM_ANON, O_RDWR | O_CLOEXEC, 0600);
+#else
+    char name[] = "/tmp/fuzzel-wayland-shm-buffer-pool-XXXXXX";
+    pool_fd = mkostemp(name, O_CLOEXEC);
+    unlink(name);
+#endif
     if (pool_fd == -1) {
         LOG_ERRNO("failed to create SHM backing memory file");
         goto err;
