90df36c1c0662e7c7a7225ad8fb502847dd15fc4
Daniel Eklöf <daniel@ekloef.se>
2022-04-12 16:56:02 +0200

    wayland: drop members already available in ‘conf’

--- wayland.c
+++ wayland.c
@@ -144,6 +144,7 @@ struct seat {
 };
 
 struct wayland {
+    const struct config *conf;
     struct fdm *fdm;
     struct render *render;
     struct prompt *prompt;
@@ -162,13 +163,10 @@ struct wayland {
     int scale;
     float dpi;
     enum fcft_subpixel subpixel;
-    enum dpi_aware dpi_aware;
     bool font_is_sized_by_dpi;
 
     enum { KEEP_RUNNING, EXIT_UPDATE_CACHE, EXIT} status;
     int exit_code;
-    enum dmenu_mode dmenu_mode;
-    const char *launch_prefix;
 
     struct wl_callback *frame_cb;
     struct buffer *pending;
@@ -182,7 +180,6 @@ struct wayland {
     struct wl_shm *shm;
     struct zxdg_output_manager_v1 *xdg_output_manager;
 
-    char *output_name;
     tll(struct monitor) monitors;
     const struct monitor *monitor;
 
@@ -442,11 +439,12 @@ execute_selected(struct wayland *wayl)
     struct application *app = match != NULL ? match->application : NULL;
     ssize_t index = match != NULL ? match->index : -1;
 
-    if (wayl->dmenu_mode != DMENU_MODE_NONE) {
-        dmenu_execute(app, index, wayl->prompt, wayl->dmenu_mode);
+    if (wayl->conf->dmenu.enabled != DMENU_MODE_NONE) {
+        dmenu_execute(app, index, wayl->prompt, wayl->conf->dmenu.mode);
         wayl->exit_code = EXIT_SUCCESS;
     } else {
-        bool success = application_execute(app, wayl->prompt, wayl->launch_prefix);
+        bool success = application_execute(
+            app, wayl->prompt, wayl->conf->launch_prefix);
         wayl->exit_code = success ? EXIT_SUCCESS : EXIT_FAILURE;
 
         if (success && match != NULL) {
@@ -874,7 +872,7 @@ guess_subpixel(const struct wayland *wayl)
 static bool
 size_font_using_dpi(const struct wayland *wayl)
 {
-    switch (wayl->dpi_aware) {
+    switch (wayl->conf->dpi_aware) {
     case DPI_AWARE_NO: return false;
     case DPI_AWARE_YES: return true;
 
@@ -1146,9 +1144,9 @@ xdg_output_handle_name(void *data, struct zxdg_output_v1 *xdg_output,
     free(mon->name);
     mon->name = name != NULL ? strdup(name) : NULL;
 
-    if (wayl->output_name != NULL &&
+    if (wayl->conf->output != NULL &&
         mon->name != NULL &&
-        strcmp(wayl->output_name, mon->name) == 0)
+        strcmp(wayl->conf->output, mon->name) == 0)
     {
         wayl->monitor = mon;
     }
@@ -1648,20 +1646,17 @@ wayl_init(const struct config *conf, struct fdm *fdm,
 {
     struct wayland *wayl = malloc(sizeof(*wayl));
     *wayl = (struct wayland){
+        .conf = conf,
         .fdm = fdm,
         .render = render,
         .prompt = prompt,
         .matches = matches,
         .status = KEEP_RUNNING,
         .exit_code = !conf->dmenu.enabled ? EXIT_SUCCESS : EXIT_FAILURE,
-        .dmenu_mode = conf->dmenu.enabled ? conf->dmenu.mode : DMENU_MODE_NONE,
-        .launch_prefix = conf->launch_prefix,
-        .output_name = conf->output != NULL ? strdup(conf->output) : NULL,
         .font_reloaded = {
             .cb = font_reloaded_cb,
             .data = data,
         },
-        .dpi_aware = conf->dpi_aware,
     };
 
     parse_font_spec(conf->font, &wayl->font_count, &wayl->fonts);
@@ -1835,7 +1830,6 @@ wayl_destroy(struct wayland *wayl)
         free(wayl->fonts[i].pattern);
     free(wayl->fonts);
 
-    free(wayl->output_name);
     free(wayl);
 }
 
