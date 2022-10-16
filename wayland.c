1ad676e36020192a823da0ef707862aa41beb08b
Daniel Eklöf <daniel@ekloef.se>
2022-10-16 15:18:36 +0200

    wayland: use layer from config

--- wayland.c
+++ wayland.c
@@ -1777,7 +1777,7 @@ wayl_init(const struct config *conf, struct fdm *fdm,
     wayl->layer_surface = zwlr_layer_shell_v1_get_layer_surface(
         wayl->layer_shell, wayl->surface,
         wayl->monitor != NULL ? wayl->monitor->output : NULL,
-        ZWLR_LAYER_SHELL_V1_LAYER_TOP, "launcher");
+        conf->layer, "launcher");
 
     if (wayl->layer_surface == NULL) {
         LOG_ERR("failed to create layer shell surface");
