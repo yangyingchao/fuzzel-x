98f05dc28e81d9e8bbad1bdb11aa2ec9beb306e2
Daniel Eklöf <daniel@ekloef.se>
2022-11-03 15:22:29 +0100

    render: add render_icon_size() function; returns the icon size to use

--- render.h
+++ render.h
@@ -28,3 +28,5 @@ void render_prompt(
 void render_match_list(
     const struct render *render, struct buffer *buf,
     const struct prompt *prompt, const struct matches *matches);
+
+int render_icon_size(const struct render *render);
