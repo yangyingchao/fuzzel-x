a18306f01810206383b13064b2ef6b39d76fd3eb
Daniel Eklöf <daniel@ekloef.se>
2022-12-04 20:03:41 +0100

    main: add --dmenu0 command line option
    Works like --dmenu, except that input is NULL separated instead of
    newline separated. Note that in this mode, icons are not
    supported (since Rofi’s extended icon protocol uses \0 as entry
    vs. icon delimiter).

--- dmenu.h
+++ dmenu.h
@@ -6,6 +6,6 @@
 #include "config.h"
 #include "prompt.h"
 
-void dmenu_load_entries(struct application_list *applications, int abort_fd);
+void dmenu_load_entries(struct application_list *applications, char delim, int abort_fd);
 bool dmenu_execute(const struct application *app, ssize_t index,
                    const struct prompt *prompt, enum dmenu_mode format);
