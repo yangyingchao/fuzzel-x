d8fadb1438a29b745605fbcd04afead5daa95f0c
Daniel Eklöf <daniel@ekloef.se>
2024-07-18 08:01:00 +0200

    dmenu: don't use getdelim() to read lines from stdin
    We mark stdin as non-blocking. This causes getdelim() to fail if we
    receive input on stdin, but no delimiter. For example:
    
        printf("a line without a newline");
        sleep(5);
        printf("\n");
        printf("part of the");
        sleep(5);
        printf(" next line\n");
    
    Solution: roll our own getdelim() that reads directly from the stdin
    file descriptor, and buffers data locally.
    
    In short:
    
    * When poll() says there's data to read, try to fill up the local
      buffer. If the local buffer is already full, increase its size.
    * Consume as many lines as possible from the buffer.
    * Any remaining data (i.e. a trailing partial line) is memmoved() to
      the beginning of the buffer, and we then wait for poll() to signal
      input availability.
    * If stdin is closed, treat all remaining data as a line, and consume it.

--- dmenu.c
+++ dmenu.c
@@ -23,8 +23,14 @@ dmenu_load_entries(struct application_list *applications, char delim,
 {
     tll(struct application) entries = tll_init();
 
-    char *line = NULL;
-    size_t alloc_size = 0;
+    size_t size = 0;
+    size_t alloc_size = 256;
+    char *buffer = malloc(alloc_size);
+
+    if (buffer == NULL) {
+        LOG_ERRNO("failed to allocate input buffer");
+        return;
+    }
 
     int flags;
     if ((flags = fcntl(STDIN_FILENO, F_GETFL)) < 0 ||
@@ -56,49 +62,120 @@ dmenu_load_entries(struct application_list *applications, char delim,
         if (fds[1].revents & (POLLIN | POLLHUP))
             break;
 
-        ssize_t len = getdelim(&line, &alloc_size, delim, stdin);
+        /* Increase size of input buffer, if necessary */
+        if (size >= alloc_size - 2) {
+            LOG_DBG("increasing input buffer size %zu -> %zu",
+                    alloc_size, alloc_size * 2);
 
-        if (len < 0) {
-            if (errno != 0)
-                LOG_ERRNO("failed to read from stdin");
+            alloc_size *= 2;
+
+            char *new_buf = realloc(buffer, alloc_size);
+            if (new_buf == NULL) {
+                LOG_ERRNO("failed to reallocate input buffer");
+                break;
+            }
+
+            buffer = new_buf;
+        }
+
+        ssize_t bytes_read = read(
+            STDIN_FILENO, &buffer[size], alloc_size - size - 1);
+
+        if (bytes_read < 0) {
+            if (errno == EINTR)
+                continue;
+            LOG_ERRNO("failed to read from stdin");
+            break;
+        } else if (bytes_read == 0 && size == 0) {
+            /* No more data on stdin, and all buffered data consumed */
             break;
         }
 
-        while (len > 0 && line[len - 1] == '\n')
-            line[--len] = '\0';
+        size += bytes_read;
 
         /*
-         * Support Rofi’s extended dmenu protocol. One can specify an
-         * icon by appending ‘\0icon\x1f<icon-name>’ to the entry:
+         * Consume as much as possible from the input buffer
          *
-         *  “hello world\0icon\x1ffirefox”
+         * Input buffer may contain many lines, resulting in multiple
+         * applications being added.
+         *
+         * Input buffer may not even contain a single line, in which
+         * case we can't consume anything at all.
+         *
+         * Input buffer may contain one or more full lines, followed
+         * by a final partial line. If stdin has been closed
+         * (i.e. there's no more input to read), then treat the last,
+         * partial line as a complete line (i.e. pretend it's followed
+         * by a delimiter). But, if stdin is still open, don't consume
+         * the last, partial line. Instead, wait for more input.
          */
-        const char *icon_name = NULL;
-        const char *extra = strchr(line, '\0');
-        if (extra != NULL && strncmp(extra + 1, "icon", 4) == 0) {
-            char *separator = strchr(extra + 1, '\x1f');
-            if (separator != NULL) {
-                *separator = '\0';
-                icon_name = separator + 1;
+        while (size > 0) {
+            char *delim_at = memchr(buffer, delim, size);
+            if (delim_at == NULL) {
+                if (bytes_read > 0) {
+                    /* No delimiter yet, wait for more data */
+                    LOG_DBG("no delimiter found, waiting for more data...");
+                    break;
+                } else {
+                    /* No delimiter, but stdin has been closed. Treat
+                       the last chunk of data as a complete line. */
+                    LOG_DBG("last line: pointing delim_at to the end of the buffer");
+                    delim_at = &buffer[size];
+                    size++;
+                }
             }
+
+            const size_t entry_len = delim_at - buffer;
+            *delim_at = '\0';
+
+            /*
+             * Support Rofi’s extended dmenu protocol. One can specify
+             * an icon by appending ‘\0icon\x1f<icon-name>’ to the
+             * entry:
+             *
+             *  “hello world\0icon\x1ffirefox”
+             */
+            char *icon_name = NULL;
+            const char *extra = memchr(buffer, '\0', entry_len);
+
+            if (extra != NULL) {
+                const size_t extra_len = delim_at - extra;
+
+                /*
+                 * 'extra' is "\0icon\x1f" - 6 characters. Require
+                 * *more* than 6, since icon *name* cannot be empty
+                 */
+                if (extra_len > 6 &&  memcmp(extra, "\0icon\x1f", 6) == 0)
+                    icon_name = strndup(extra + 6, delim_at - (extra + 6));
+            }
+
+            LOG_DBG("%s (icon=%s)", buffer, icon_name);
+
+            char32_t *wline = ambstoc32(buffer);
+
+            /* Consume entry from input buffer */
+            const size_t consume = delim_at + 1 - buffer;
+            assert(consume <= size);
+
+            memmove(buffer, delim_at + 1, size - consume);
+            size -= consume;
+
+            if (wline == NULL) {
+                free(icon_name);
+                continue;
+            }
+
+            struct application app = {
+                .title = wline,
+                .icon = {.name = icon_name},
+                .visible = true,
+            };
+
+            tll_push_back(entries, app);
         }
-
-        LOG_DBG("%s (icon=%s)", line, icon_name);
-
-        char32_t *wline = ambstoc32(line);
-        if (wline == NULL)
-            continue;
-
-        struct application app = {
-            .title = wline,
-            .icon = {.name = icon_name != NULL ? strdup(icon_name) : NULL},
-            .visible = true,
-        };
-
-        tll_push_back(entries, app);
     }
 
-    free(line);
+    free(buffer);
 
 out:
 
