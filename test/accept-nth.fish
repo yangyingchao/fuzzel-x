6d2fb1e58db0cb77d0a31672c34e3b4a27ba2518
Daniel Eklöf <daniel@ekloef.se>
2025-08-01 09:57:56 +0200

    test: accept-nth: new test suite

---
+++ test/accept-nth.fish
@@ -0,0 +1,41 @@
+#!/usr/bin/fish
+
+set got
+
+function test_accept_nth --argument-names input search nth_format
+    set got (echo -e "$input" |
+                 $FUZZEL_TEST_BIN --dmenu \
+                                  --search="$search" \
+                                  --auto-select \
+                                  --accept-nth="$nth_format")
+end
+
+test_accept_nth "apple\tbanana\tcherry" "apple" "{3} - {2}: {1}"
+@test "--accept-nth with valid formatters" "$got" = "cherry - banana: apple"
+
+test_accept_nth "apple\tbanana\tcherry" "apple" "{0} - {4}"
+@test "--accept-nth with invalid column indices" "$got" = "{0} - {4}"
+
+test_accept_nth "apple\tbanana\tcherry" "apple" "{{1}"
+@test "--accept-nth with unclosed curly bracket" "$got" = "{apple"
+
+test_accept_nth "apple\tbanana\tcherry" "apple" "{1a}"
+@test "--accept-nth with non-numerical index" "$got" = "{1a}"
+
+test_accept_nth "apple\tbanana\tcherry" "apple" "{1..3}"
+@test "--accept-nth with index range" "$got" = "apple banana cherry"
+
+test_accept_nth "apple\tbanana\tcherry" "apple" "{3..1}"
+@test "--accept-nth with reversed index range" "$got" = "{3..1}"
+
+test_accept_nth "apple\tbanana\tcherry" "apple" "{1.3} {1...3}"
+@test "--accept-nth with invalid index range" "$got" = "{1.3} {1...3}"
+
+test_accept_nth "apple\tbanana\tcherry" "apple" "{2..}"
+@test "--accept-nth with open index range" "$got" = "banana cherry"
+
+test_accept_nth "apple\tbanana\tcherry" "apple" "{2.} {2...}"
+@test "--accept-nth with invalid open index range" "$got" = "{2.} {2...}"
+
+test_accept_nth "apple\tbanana\tcherry" "apple" "{..2}"
+@test "--accept-nth with invalid open index range" "$got" = "{..2}"
